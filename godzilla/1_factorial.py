17ac7c8f05aefa65a7d894dd2ea57df8c2aeb0ad
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-07-12 00:02:37 -0300

    Refactor is coming

--- godzilla/1_factorial.py
+++ godzilla/1_factorial.py
@@ -1,10 +1,12 @@
 # Ejercicio 1.3
 def iterador():
-    for i in range(5): # Multiplicacion de iterador por iterador
+    # Multiplicacion de iterador por iterador
+    for i in range(5): 
         print(i * i)
 
 def raiz():
-    for i in range(2, 6): # Raiz cuadrada segun iterador
+    # Raiz cuadrada segun iterador
+    for i in range(2, 6): 
         print(i, 2 **i)
 
 def factorial(num):
--- random/distancia.py
+++
@@ -1,7 +0,0 @@
-from math import sqrt
-
-def dist(x1, y1, x2, y2, x3, y3):
-    ab = sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
-    bc = sqrt( (x2 - x3)**2 + (y2 - y3)**2 )
-
-    return ab + bc / 2
