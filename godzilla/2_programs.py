9c52ab58108ff01b8f086440220b367c12635c80
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-08-05 16:14:02 -0300

    Refactor

--- godzilla/2_programs.py
+++ godzilla/2_programs.py
@@ -14,20 +14,35 @@ def credit():
 
 # EXERCISE 2.2
 
-# Toma una temperatura en Fahrenheit y retorna en Celsius
+# Toma una temperatura en Fahrenheit y retorna en Celsius y viceversa.
 class Temperature:
     def __init__(self):
         print("Welcome to the converter of temperature.")
 
-    def fahrenheit(self, num):
+    # Fahrenheit to Celsius
+    def fahrenheit_to_celsius(self, num):
         self.num = num
 
         celsius = (num - 32) * 5/9
         return celsius
+    
+    # Celsius to Fahrenheit
+    def celsius_to_fahrenheit(self, num):
+        self. num = num
 
-# INSTANCE
+        fahrenheit = (num * 1.8) + 32
+        return fahrenheit
+
+
+# Instance
 test = Temperature()
-print(f"La temperatura introducida en Fahrenheit es {test.fahrenheit(5)} Celsius.")
+
+# Fahrenheit to Celsius
+celsius = test.fahrenheit_to_celsius(24)
+# Celsius to Fahrenheit
+fahrenheit = test.celsius_to_fahrenheit(celsius)
+# Result
+print(f"{celsius} °C son {fahrenheit} °F")
 
 
 
@@ -36,5 +51,5 @@ print(f"La temperatura introducida en Fahrenheit es {test.fahrenheit(5)} Celsius
 
 # Devuelve valores en Celsius de 0 a 120 Fahrenheit con una franja de 10 grados F°.
 for i in range (0, 120, 10):
-    print(f"{i} Fahrenheit = {test.fahrenheit(i)} Celsius.")
+    print(f"{i} Fahrenheit = {test.fahrenheit_to_celsius(i)} Celsius.")
     
