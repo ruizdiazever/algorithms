9b345c26f783e227fc0fd14c5fa673cd25a1bcfd
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-08-07 21:12:25 -0300

    Add induccion matematica

--- godzilla/2_programs.py
+++ godzilla/2_programs.py
@@ -66,10 +66,55 @@ def pairs():
             else:
                 print(i+1)
 
-# Instance
-pares = pairs()
-
 
 
 
 # EXERCISE 2.5
+from time import time
+def count_elapsed_time(f):
+    """
+    Decorator.
+    Execute the function and calculate the elapsed time.
+    Print the result to the standard output.
+    """
+    def wrapper():
+        # Start counting.
+        start_time = time()
+        # Take the original function's return value.
+        ret = f()
+        # Calculate the elapsed time.
+        elapsed_time = time() - start_time
+        print("Elapsed time: %0.10f seconds." % elapsed_time)
+        return ret
+    
+    return wrapper
+
+
+
+# Induccion matematica, suma de los números naturales desde 1 hasta num
+# Algoritmo original propio
+@count_elapsed_time
+def triangule_numbers():
+    #num = int(input("Please input one integer number: "))
+    num = 5
+    lista = []
+
+    for i in range(1, num + 1, 1):
+        lista.append(i)
+        print(f"{i} - {sum(lista)}")
+
+# Instance
+test = triangule_numbers()
+
+# Algoritmo basado en ecuacion
+@count_elapsed_time
+def triangule_numbers_2():
+    #num = int(input("Please input one integer number: "))
+    num = 5
+
+    for i in range(1, num + 1, 1):
+        print(f"{i} - {int(i * (i + 1) / 2)}")
+
+
+# Instance
+guy = triangule_numbers_2()
